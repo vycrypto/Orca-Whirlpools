1bfcbabe88cd9fa5def9a2fae84a94ded0fc7452
pd123459 <47801063+pd123459@users.noreply.github.com>
2023-01-18 20:59:36 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -31,6 +31,14 @@ st.markdown(
         in specific price ranges. Such innovation not only encourages a more efficient use of capital, 
         but also reduces slippage costs for traders.
         
+        #### **Key takeaways:**
+        
+        1. The SOL-USDC pool has generated the most fees for liquidity providers in 2022
+        
+        2. $BONK pools generated the most trading volume and fees in 2023 
+        
+        3. Liquidity providers adjust their positions much more frequently on Orca compared to Uniswap, contributing to better capital efficiency in Orca whirlpools
+        
 """)
 st.markdown(
     f'####  ORCA Token Price[{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True
@@ -38,35 +46,56 @@ st.markdown(
 st.write("""*The price of the $ORCA token. Currency in USD.*""")
 st.plotly_chart(f.fig_hist_prc, use_container_width=True)
 
-
 st.markdown(
     """
     ---
     # Growth Over Time
-    Since launch in March 2022, the SOL-USDC whirlpool has ranked high 
-    regardless by trading volume, fees generated for liquidity providers, or daily number of 
-    transactions. As a result, the SOL-USDC pool had always been a favorite among liquidity 
-    providers in 2022. 
     """
 )
 
-#n1, n2 = st.columns((1, 1))
 
-st.markdown(f'#### Daily Number of Transactions per Whirlpool [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
+st.markdown(f'#### Daily Active Whirlpools [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
 st.write(
-    '*Number of unique transactions interacting with each Orca Whirlpool each day.*')
-st.plotly_chart(f.fig_trx_pool, use_container_width=True)
+    """*The number of Orca Whirlpools that have active transactions per day.* """)
+st.plotly_chart(f.fig_pool_count, use_container_width=True)
 
-st.markdown(f'#### Active Liquidity Providers per Whirlpool [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/d8efa2d6-ba9c-4610-ae0b-bf2bcbe80b7c)', unsafe_allow_html=True)
-st.write(
-    """*Number of unique liquidity providers who increased or decreased liquidity from each Orca Whirlpool each day.*""")
-st.plotly_chart(f.fig_lp_num_pool, use_container_width=True)
-
+st.markdown(
+    """
+    
+    From March 2022 to January 2023, the number of daily active Orca whirlpools has increased from 
+    single digit to over 100 with more than 400 whirlpools in total. Furthermore, the daily trading 
+    volume in January surged to the 5-10 million USD range with the maximum daily trading volume 
+    surpassing 15 million USD. The rise in 2023 usage and adoption was impacted by a newly launched token $BONK
+    which we will dive into in the section below. This section focuses on the growth of Orca 
+    whirlpools in 2022.
+    
+    The most popular whirlpool in 2022 is the SOL-USDC pool, often contributing more than 50% of the total 
+    whirlpool trading volume and fees earned by liquidity providers on a daily basis. As a result, it 
+    also has the highest number of active liquidity providers on most days throughout 2022. While most 
+    volume concentrates in the SOL-USDC pool, the distribution of active liquidity providers is more 
+    decentralized. For example, while the SOL-USDC pool contributed to 73.2% of total whirlpool trading volume
+    on April 12th, 2022, only 44.4% of active liquidity providers were interacting with the SOL-USDC pool. 
+    Such difference in distribution potentially indicates the liquidity providers for Orca whirlpools have 
+    motivations beyond earning trading fees. 
+    
+    mSOL(*a liquid staking token that you receive when you stake SOL on the Marinade protocol*) and 
+    stSOL(*a token that represents staking positions with a diverse set of professional validators on Solana*) 
+    whirlpools have also been popular in 2022. 
+    
+    *Note: pie charts below are zoomed in views of the data on the following four selected dates - 4/12/22
+    , 7/18/22, 11/18/22, 1/5/23.*
+    """
+)
 
 st.markdown(f'#### Total Trading Volume (USD) [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
 st.write(
-    """*Total volume for swaps for each pool per day.* """)
+    """*Total trading volume for each pool per day.* """)
 st.plotly_chart(f.fig_swap_vol_pool, use_container_width=True)
+n1, n2= st.columns((1,1))
+n1.plotly_chart(f.fig_pie_apr, use_container_width=True)
+n2.plotly_chart(f.fig_pie_jul, use_container_width=True)
+n1.plotly_chart(f.fig_pie_nov, use_container_width=True)
+n2.plotly_chart(f.fig_pie_jan, use_container_width=True)
 
 
 st.markdown(f'#### Daily Fee Earned by Liquidity Providers (USD) [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
@@ -75,12 +104,38 @@ st.write(
     3% goes to the Foundation.* """)
 st.plotly_chart(f.fig_swap_fee_pool, use_container_width=True)
 
+g1, g2= st.columns((1,1))
+g1.plotly_chart(f.fig_pie_fee_apr, use_container_width=True)
+g2.plotly_chart(f.fig_pie_fee_jul, use_container_width=True)
+g1.plotly_chart(f.fig_pie_fee_nov, use_container_width=True)
+g2.plotly_chart(f.fig_pie_fee_jan, use_container_width=True)
+
+
+st.markdown(f'#### Active Liquidity Providers per Whirlpool [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/d8efa2d6-ba9c-4610-ae0b-bf2bcbe80b7c)', unsafe_allow_html=True)
+st.write(
+    """*Number of unique liquidity providers who increased or decreased liquidity from each Orca Whirlpool each day.*""")
+st.plotly_chart(f.fig_lp_num_pool, use_container_width=True)
+
+m1, m2= st.columns((1,1))
+m1.plotly_chart(f.fig_pie_lp_apr, use_container_width=True)
+m2.plotly_chart(f.fig_pie_lp_jul, use_container_width=True)
+m1.plotly_chart(f.fig_pie_lp_nov, use_container_width=True)
+m2.plotly_chart(f.fig_pie_lp_jan, use_container_width=True)
+
+
+
+
 
 st.markdown(
     """
     ---
     # Impact of $BONK on Orca Whirlpools
-    On January 3rd 2023, $BONK was launched, which changed the landscapes on Orca whirlpools.
+    
+    While the SOL-USDC pool dominated trading volume in most of 2022, the launch of BONK token changed
+    the Orca whirlpools landscape. As the first dog-themed meme coin on Solana, $BONK was up more than 30,000%
+    in the first week of January. The launch not only drove the price of SOL from 8 to 22 USD, but also 
+    changed the popularity of Orca whirlpools. 
+    
     """
 )
 
@@ -89,30 +144,23 @@ st.write(
     """*Total volume for swaps for each pool per day.*  """)
 st.plotly_chart(f.fig_swap_vol_pool_bonk, use_container_width=True)
 
-st.markdown(f'#### Daily Active Liquidity Providers (USD) [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
-st.write(
-    """*Number of unique liquidity providers who increased or decreased liquidity from each Orca Whirlpool each day.*""")
-st.plotly_chart(f.fig_lp_pool_bonk, use_container_width=True)
-
 st.markdown(f'#### Total Fees Earned by Liquidity Providers (USD) [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
 st.write(
     """ *Total fees earned by liquidity providers for each pool per day. Only 97% of the fees counted as 
     3% goes to the Foundation.*""")
 st.plotly_chart(f.fig_swap_fee_pool_bonk, use_container_width=True)
 
-
-
+st.markdown(f'#### Daily Active Liquidity Providers [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3)', unsafe_allow_html=True)
+st.write("""*Number of unique liquidity providers who increased or decreased liquidity from each Orca Whirlpool each day.*""")
+st.plotly_chart(f.fig_lp_pool_bonk, use_container_width=True)
 
 st.markdown(
     """
     ---
     # Capital Efficiency: Orca vs. Uniswap
-    
     With concentrated liquidity, liquidity providers on Orca Whirlpools rebalance their positions much more frequently 
-    compared to liquidity providers on Uniswap. 
-
-
-
+    compared to liquidity providers on Uniswap. While an average Uniswap liquidity provider adjusts his/her 
+    positions 3-4 times a week, an average Orca liquidity provider adjusts his/her positions everyday. 
     """
 )
 
@@ -121,14 +169,14 @@ st.markdown(
     f'#### Daily LP Position Adjustment on Orca [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/2ec0ad61-7b05-4a13-82a0-423d39dc0e6f)', unsafe_allow_html=True
 )
 st.write("""*The average daily number of price range adjustments a liquidity provider performs. Estimated by 
-counting the pairs of decreasing liquidity transaction with increasing liquidity transaction in the same whirlpool each day.*""")
+counting the pairs of decreasing liquidity transaction with increasing liquidity transaction in the same whirlpool by each liquidity provider per day.*""")
 st.plotly_chart(f.fig_lp_adj_orca, use_container_width=True)
 
 st.markdown(
     f'####  Daily LP Position Adjustment on Uniswap [{icons.setting_icon}](https://app.flipsidecrypto.com/velocity/queries/65d08749-498e-4406-aa54-62ab91b210b5)', unsafe_allow_html=True
 )
 st.write("""*The average daily number of price range adjustments a liquidity provider performs. Estimated by 
-counting the pairs of decreasing liquidity transaction with increasing liquidity transaction in the same whirlpool each day.*""")
+counting the pairs of decreasing liquidity transaction with increasing liquidity transaction in the same whirlpool by each liquidity provider per day.*""")
 st.plotly_chart(f.fig_lp_adj_uni, use_container_width=True)
 
 
--- figures.py
+++ figures.py
@@ -64,34 +64,100 @@ def chart_update_layout(figure, x_axis, y_axis):
 
 # ============================ HISTORICAL DAILY TIME SERIES ===============================
 ## ------ DATA ----------
+
+'''
+def filter_pool(x):
+    if 'SOL-USDC' in x:
+        return ('Known Pool')
+    elif 'mSOL-USDC' in x:
+        return ('Known pool')
+    elif 'BTC-USDC' in x:
+        return ('Known pool')
+    elif 'stSOL' in x:
+        return ('stSOL pools')
+    elif 'UST-USDC' in x:
+        return ('Known pool')
+    elif 'SOL-mSOL' in x:
+        return ('Known pool')
+    elif 'mSOL-USDT' in x:
+        return ('Known pool')
+    elif 'KI-USDC' in x:
+        return ('Known pool')
+    elif 'CAVE-USDC' in x:
+        return ('Known pool')
+    elif 'ORCA-USDC' in x:
+        return ('Known pool')
+    elif 'ETH-USDC' in x:
+        return ('Known pool')
+    elif ('USDC-MEDIA' in x) or ('MEDIA-USDC' in x):
+        return ('Known pool')
+    elif ('bonk-sol' in x) or ('SOL-BONK' in x):
+        return ('Known pool')
+    elif ('bonk-usdc' in x) or ('BONK-USDC' in x):
+        return ('Known pool')
+    elif 'pool' in x:
+        return(x)
+    else:
+        return ('Other pool')
+        
+'''
+
+def filter_pool_pie(x):
+    if 'pool' in x:
+        return(x)
+    else:
+        return ('Other pool')
+
+def filter_pool(x):
+    if 'SOL-USDC' in x:
+        return ('SOL-USDC Pool')
+    elif 'mSOL-USDC' in x:
+        return ('mSOL-USDC pool')
+    elif 'BTC-USDC' in x:
+        return ('BTC-USDC pool')
+    elif 'USDC-MEDIA' in x:
+        return ('USDC-MEDIA pool')
+    elif 'stSOL' in x:
+        return ('stSOL pools')
+    elif 'UST-USDC' in x:
+        return ('UST-USDC pool')
+    elif 'SOL-mSOL' in x:
+        return ('SOL-mSOL pool')
+    elif 'mSOL-USDT' in x:
+        return ('mSOL-USDT pool')
+    elif 'ORCA-USDC' in x:
+        return ('ORCA-USDC pool')
+    elif 'ETH-USDC' in x:
+        return ('ETH-USDC pool')
+    elif ('bonk-sol' in x) or ('SOL-BONK' in x):
+        return ('BONK-SOL pool')
+    elif ('bonk-usdc' in x) or ('BONK-USDC' in x):
+        return ('BONK-USDC pool')
+    #elif 'pool' in x:
+        #return(x)
+    else:
+        return ('Other pool')
+
+
+def filter_bonk(x):
+    if ('bonk-sol' in x) or ('SOL-BONK' in x):
+        return ('BONK-SOL pool')
+    elif ('bonk-usdc' in x) or ('BONK-USDC' in x):
+        return ('BONK-USDC pool')
+    elif 'SOL-USDC' in x:
+        return ('SOL-USDC Pool')
+    else:
+        return('Other pool')
+
+# ------------------------ LP Daily Position Adj. ------------------------------------
+## Data Frame
 UniLP = 'https://node-api.flipsidecrypto.com/api/v2/queries/65d08749-498e-4406-aa54-62ab91b210b5/data/latest'
 df_unilp_ttl = pd.read_json(UniLP)
-
-OrcaLPSplit = 'https://node-api.flipsidecrypto.com/api/v2/queries/d8efa2d6-ba9c-4610-ae0b-bf2bcbe80b7c/data/latest'
-df_orcalp_splitpool = pd.read_json(OrcaLPSplit)
-df_orcalp_bonk = df_orcalp_splitpool.loc[:'2023-01-01 00:00:00.000']
-df_orcalp_bonk['POOL BONK?'] = df_orcalp_bonk['POOL_ADDRESS_NAME'].str.contains("BONK")
+df_unilp_ttl['AVG_LP_ADJ_MA'] = df_unilp_ttl['AVG_LP_ADJ'][::-1].rolling(7).mean()[::-1].replace(np.nan, 'None')
 
 OrcaLPTtl = 'https://node-api.flipsidecrypto.com/api/v2/queries/2ec0ad61-7b05-4a13-82a0-423d39dc0e6f/data/latest'
 df_orcalp_ttl = pd.read_json(OrcaLPTtl)
-
-OrcaDailyHist = 'https://node-api.flipsidecrypto.com/api/v2/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3/data/latest'
-df_orca_daily = pd.read_json(OrcaDailyHist)
-df_orca_daily.loc[df_orca_daily['NUM_TRX'] <= 50, 'POOL NAME'] = 'OTHER'
-df_orca_daily.loc[df_orca_daily['NUM_TRX'] > 50, 'POOL NAME'] = df_orca_daily['POOL_ADDRESS_NAME']
-
-
-#df_orca_daily['2023'] = df_orca_daily['DT'].str.contains("2023", regex=False)
-df_orca_bonk = df_orca_daily.loc[:'2023-01-01 00:00:00.000']
-df_orca_bonk['POOL BONK?'] = df_orca_bonk['POOL_ADDRESS_NAME'].str.contains("BONK", regex=False)
-
-
-df_unilp_ttl['AVG_LP_ADJ_MA'] = df_unilp_ttl['AVG_LP_ADJ'][::-1].rolling(7).mean()[::-1].replace(np.nan, 'None')
-
 df_orcalp_ttl['AVG_LP_ADJ_MA'] = df_orcalp_ttl['AVG_PRICERANGE_ADJ'][::-1].rolling(7).mean()[::-1].replace(np.nan, 'None')
-
-#df_orca_daily[''] = df_unilp_ttl['AVG_LP_ADJ'][::-1].rolling(7).mean()[::-1].replace(np.nan, 'None')
-
 
 ## ------ PLOTS ----------
 # --> LP avg adj (UNI)
@@ -111,44 +177,147 @@ fig_lp_adj_orca.add_bar(x=df_orcalp_ttl['DT'],y=df_orcalp_ttl['AVG_PRICERANGE_AD
 chart_update_layout(fig_lp_adj_orca, "", "Daily LP Position Adjustment on Orca")
 
 
+#------------------ ORCA LP Stats By Pool -----------------------------------------
+## Data Frame
+OrcaLPSplit = 'https://node-api.flipsidecrypto.com/api/v2/queries/d8efa2d6-ba9c-4610-ae0b-bf2bcbe80b7c/data/latest'
+df_orcalp_splitpool = pd.read_json(OrcaLPSplit)
+df_orcalp_splitpool['Pool Name'] = df_orcalp_splitpool['POOL_ADDRESS_NAME'].apply(filter_pool)
+
+bonk_index = df_orcalp_splitpool.index[df_orcalp_splitpool['DT'] == '2022-12-15 00:00:00.000'].tolist()
+df_orcalp_bonk = df_orcalp_splitpool.loc[:bonk_index[-1]]
+df_orcalp_bonk['Pool Name Type'] = df_orcalp_bonk['POOL_ADDRESS_NAME'].apply(filter_bonk)
+
+df_orcalp_apr = df_orcalp_splitpool[df_orcalp_splitpool['DT'].str.contains("2022-04-12")]
+df_orcalp_apr['Pool Name'] = df_orcalp_apr['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orcalp_jul = df_orcalp_splitpool[df_orcalp_splitpool['DT'].str.contains("2022-07-18")]
+df_orcalp_jul['Pool Name'] = df_orcalp_jul['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orcalp_nov = df_orcalp_splitpool[df_orcalp_splitpool['DT'].str.contains("2022-11-18")]
+df_orcalp_nov['Pool Name'] = df_orcalp_nov['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orcalp_jan = df_orcalp_splitpool[df_orcalp_splitpool['DT'].str.contains("2023-01-05")]
+df_orcalp_jan['Pool Name'] = df_orcalp_jan['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+
 #--> Active LP (Orca)
-fig_lp_num_pool = px.bar(df_orcalp_splitpool,x='DT',y='NUM_USERS',color='POOL_ADDRESS_NAME'
-                           , hover_data={'DT': False, 'NUM_USERS': True}
+fig_lp_num_pool = px.bar(df_orcalp_splitpool,x='DT',y='NUM_USERS',color='Pool Name'
+                           , hover_data={'DT': True, 'NUM_USERS': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Number of Active LPs'))
-fig_lp_pool_bonk = px.bar(df_orcalp_bonk,x='DT',y='NUM_USERS',color='POOL BONK?'
-                           , hover_data={'DT': False, 'NUM_USERS': True}
+fig_lp_pool_bonk = px.bar(df_orcalp_bonk,x='DT',y='NUM_USERS',color='Pool Name Type'
+                          , hover_data={'DT': True, 'NUM_USERS': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Number of Active LPs'))
 
+# Pies
+fig_pie_lp_apr = px.pie(df_orcalp_apr,names='Pool Name',values='NUM_USERS', title = 'April 2022').update_traces(textposition='inside')
+fig_pie_lp_apr.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_lp_jul = px.pie(df_orcalp_jul,names='Pool Name',values='NUM_USERS', title = 'July 2022').update_traces(textposition='inside')
+fig_pie_lp_jul.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_lp_nov = px.pie(df_orcalp_nov,names='Pool Name',values='NUM_USERS', title = 'November 2022').update_traces(textposition='inside')
+fig_pie_lp_nov.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_lp_jan = px.pie(df_orcalp_jan,names='Pool Name',values='NUM_USERS', title = 'January 2023').update_traces(textposition='inside')
+fig_pie_lp_jan.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+
+
+#------------------ ORCA Total Stats By Pool -----------------------------------------
+## Data Frame
+OrcaDailyHist = 'https://node-api.flipsidecrypto.com/api/v2/queries/860f8493-6bad-45a6-98b0-d76d587dc1c3/data/latest'
+df_orca_daily = pd.read_json(OrcaDailyHist)
+df_orca_daily.loc[df_orca_daily['NUM_TRX'] <= 50, 'POOL NAME'] = 'OTHER'
+df_orca_daily.loc[df_orca_daily['NUM_TRX'] > 50, 'POOL NAME'] = df_orca_daily['POOL_ADDRESS_NAME']
+df_orca_daily['Pool Name'] = df_orca_daily['POOL_ADDRESS_NAME'].astype(str).apply(filter_pool)
+
+df_orca_count = df_orca_daily.groupby('DT').count()
+df_orca_count['POOL_COUNT_MA'] = df_orca_count['POOL_ADDRESS_NAME'][::1].rolling(7).mean()[::1].replace(np.nan, 'None')
+df_orca_count['DT']=df_orca_count.index
+
+bonk_index_ttl = df_orca_daily.index[df_orca_daily['DT'] == '2022-12-15 00:00:00.000'].tolist()
+df_orca_bonk = df_orca_daily.loc[:bonk_index_ttl[-1]]
+df_orca_bonk['Pool Name Type'] = df_orca_bonk['POOL_ADDRESS_NAME'].astype(str).apply(filter_bonk)
+
+df_orca_apr = df_orca_daily[df_orca_daily['DT'].str.contains("2022-04-12")]
+df_orca_apr['Pool Name'] = df_orca_apr['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orca_jul = df_orca_daily[df_orca_daily['DT'].str.contains("2022-07-18")]
+df_orca_jul['Pool Name'] = df_orca_jul['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orca_nov = df_orca_daily[df_orca_daily['DT'].str.contains("2022-11-18")]
+df_orca_nov['Pool Name'] = df_orca_nov['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
+df_orca_jan = df_orca_daily[df_orca_daily['DT'].str.contains("2023-01-05")]
+df_orca_jan['Pool Name'] = df_orca_jan['POOL_ADDRESS_NAME'].apply(filter_pool_pie)
 
 # --> $ORCA Price
 fig_hist_prc = px.line(df_orca_daily, x='DT', y='ORCA_CLOSE', labels ={'DT':'Date', 'ORCA_CLOSE':'$OP Price'}
                 ,color_discrete_sequence=['yellow']
-                ,hover_data={'DT':False,'ORCA_CLOSE':True})
+                ,hover_data={'DT':True,'ORCA_CLOSE':True})
 chart_update_layout(fig_hist_prc, "", "ORCA Token Price (USD)")
 
+# --> Active Pool Count
+fig_pool_count = px.line(df_orca_count, x='DT', y='POOL_COUNT_MA', labels ={'DT':'Date', 'POOL_COUNT_MA':'7-Day MA'}
+                ,color_discrete_sequence=['white']
+                ,hover_data={'DT':False,'POOL_COUNT_MA':True} )
+fig_pool_count.add_bar(x=df_orca_count['DT'],y=df_orca_count['POOL_ADDRESS_NAME'], name="# Active Pools / Day"
+                    ,marker_color='rgba(125,239,161,255)')
+chart_update_layout(fig_pool_count, "", "Number of Active Pools / Day")
 
 #--> daily transaction by pool
-fig_trx_pool = px.bar(df_orca_daily,x='DT',y='NUM_TRX',color='POOL NAME'
-                           , hover_data={'DT': False, 'NUM_TRX': True}
+fig_trx_pool = px.bar(df_orca_daily,x='DT',y='NUM_TRX',color='Pool Name'
+                           , hover_data={'DT': True, 'NUM_TRX': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Number of Transactions'))
 
 # Total Swap volume by pool
-fig_swap_vol_pool = px.bar(df_orca_daily,x='DT',y='TTL_SWAP_VOLUME',color='POOL NAME'
-                           , hover_data={'DT': False, 'TTL_SWAP_VOLUME': True}
-                           ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Total Trading Volume (USD)'))
-# --> (bonk)
-fig_swap_vol_pool_bonk = px.bar(df_orca_bonk,x='DT',y='TTL_SWAP_VOLUME',color='POOL BONK?'
-                           , hover_data={'DT': False, 'TTL_SWAP_VOLUME': True}
+fig_swap_vol_pool = px.bar(df_orca_daily,x='DT',y='TTL_SWAP_VOLUME',color='Pool Name'
+                           , hover_data={'DT': True, 'TTL_SWAP_VOLUME': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Total Trading Volume (USD)'))
 
+# Pies
+fig_pie_apr = px.pie(df_orca_apr,names='Pool Name',values='TTL_SWAP_VOLUME', title = 'April 2022').update_traces(textposition='inside')
+fig_pie_apr.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
 
+fig_pie_jul = px.pie(df_orca_jul,names='Pool Name',values='TTL_SWAP_VOLUME', title = 'July 2022').update_traces(textposition='inside')
+fig_pie_jul.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_nov = px.pie(df_orca_nov,names='Pool Name',values='TTL_SWAP_VOLUME', title = 'November 2022').update_traces(textposition='inside')
+fig_pie_nov.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_jan = px.pie(df_orca_jan,names='Pool Name',values='TTL_SWAP_VOLUME', title = 'January 2023').update_traces(textposition='inside')
+fig_pie_jan.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
 
 
 # Total Fee by pool
-fig_swap_fee_pool = px.bar(df_orca_daily,x='DT',y='TTL_LP_FEE',color='POOL NAME'
-                           , hover_data={'DT': False, 'TTL_LP_FEE': True}
+fig_swap_fee_pool = px.bar(df_orca_daily,x='DT',y='TTL_LP_FEE',color='Pool Name'
+                           , hover_data={'DT': True, 'TTL_LP_FEE': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Total Fee Earned by LP (USD)'))
 
-fig_swap_fee_pool_bonk = px.bar(df_orca_bonk,x='DT',y='TTL_LP_FEE',color='POOL BONK?'
-                           , hover_data={'DT': False, 'TTL_LP_FEE': True}
+fig_swap_fee_pool_bonk = px.bar(df_orca_bonk,x='DT',y='TTL_LP_FEE',color='Pool Name Type'
+                           , hover_data={'DT': True, 'TTL_LP_FEE': True}
                            ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Total Fee Earned by LP (USD)'))
+
+# Fee Pies
+fig_pie_fee_apr = px.pie(df_orca_apr,names='Pool Name',values='TTL_LP_FEE', title = 'April 2022').update_traces(textposition='inside')
+fig_pie_fee_apr.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_fee_jul = px.pie(df_orca_jul,names='Pool Name',values='TTL_LP_FEE', title = 'July 2022').update_traces(textposition='inside')
+fig_pie_fee_jul.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_fee_nov = px.pie(df_orca_nov,names='Pool Name',values='TTL_LP_FEE', title = 'November 2022').update_traces(textposition='inside')
+fig_pie_fee_nov.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+fig_pie_fee_jan = px.pie(df_orca_jan,names='Pool Name',values='TTL_LP_FEE', title = 'January 2023').update_traces(textposition='inside')
+fig_pie_fee_jan.update_layout(height=200, uniformtext_minsize=11,uniformtext_mode='hide',margin=dict(l=20, r=20, t=20, b=20))
+
+
+# --> (bonk)
+fig_swap_vol_pool_bonk = px.bar(df_orca_bonk,x='DT',y='TTL_SWAP_VOLUME',color='Pool Name Type'
+                           , hover_data={'DT': True, 'TTL_SWAP_VOLUME': True}
+                           ).update_layout(height=350, margin=dict(l=20, r=20, t=20, b=20), yaxis=dict(title='Total Trading Volume (USD)'))
+
+
+
+
+
+
+
+
+
+
+
+
